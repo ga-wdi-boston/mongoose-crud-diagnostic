3b76fc1f133bdd24022c10b8f62b15bd8ecd80ac
rdelvallej32 <rdelvallej@gmail.com>
2016-05-12 09:20:55 -0400

    response to diagnostic

--- diagnostic.js
+++ diagnostic.js
@@ -32,28 +32,61 @@ const Movement = require('./models/movement.js');
 
 /// ADD YOUR CODE BELOW
 
-const create = (name, description, startYear, endYear) => {};
+
+const create = (name, description, startYear, endYear) => {
 // Success -> console.log new Movement as JSON
 // Failure -> Console.error
 
-const index = () => {};
+Movement.create({
+    name: name,
+    description: description,
+    startYear: startYear,
+    endYear: endYear
+  }).then(function(movement){
+    console.log(movement.toJSON());
+  }).catch(console.error);
+};
+
+const index = function() {
 // Success -> console.log all Movements as JSON
 // Failure -> Console.error
+  Movement.find().then(function(movements) {
+    movements.forEach(function(movement) {
+      console.log(movement.toJSON());
+    });
+  }).catch(console.error);
+};
 
-const show = (id) => {};
+const show = (id) => {
 // Success -> If the specified Movement exists, console.log it as JSON;
 //              otherwise, console.log "Not Found" and exit.
 // Failure -> Console.error
+Movement.findById(id).then(function(movement) {
+  console.log(movement.toJSON());
+}).catch(console.error);
+};
 
-const update = (id, field, value) => {};
+const update = (id, field, value) => {
 // Success -> If the specified Movement exists, update it and console.log the
 //              updated Movement as JSON; otherwise, console.log "Not Found" and exit.
 // Failure -> Console.error
+Movement.findById(id).then(function(movement) {
+  movement[field] = value;
+  return movement.save();
+}).then(function(movement) {
+  console.log(movement.toJSON());
+}).catch(console.error);
+};
 
-const destroy = (id) => {};
+const destroy = (id) => {
 // Success -> If the specified Movement exists, destroy it and console.log 'removed';
 //              otherwise, console.log "Not Found" and exit.
 // Failure -> Console.error
+Movement.findById(id).then(function(movement) {
+    movement.remove();
+  }).catch(console.error);
+
+};
 
 module.exports = {
   create,
