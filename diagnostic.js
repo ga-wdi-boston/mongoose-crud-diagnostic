fa353fd467d153f845d7859e2f87c1f853a4591f
ddobson <ddobson445@gmail.com>
2017-03-03 09:32:18 -0500

    Complete diagnostic
    Answers in diagnostic.js

--- diagnostic.js
+++ diagnostic.js
@@ -32,25 +32,74 @@ const Movement = require('./models/movement.js');
 
 /// ADD YOUR CODE BELOW
 
-const create = (name, description, startYear, endYear) => {};
+const create = (name, description, startYear, endYear) => {
+  Movement.create({
+    'name': name,
+    'description': description,
+    'startYear': startYear,
+    'endYear': endYear
+  }).then(function (movement) {
+    console.log(JSON.stringify(movement));
+  }).catch(console.error);
+};
 // Success -> console.log new Movement as JSON
 // Failure -> Console.error
 
-const index = () => {};
+const index = () => {
+  Movement.find()
+    .then((movements) => {
+      console.log(JSON.stringify(movements));
+    })
+    .catch(console.error);
+};
 // Success -> console.log all Movements as JSON
 // Failure -> Console.error
 
-const show = (id) => {};
+const show = (id) => {
+  Movement.findById(id)
+    .then((movement) => {
+      if (movement) {
+        console.log(JSON.stringify(movement));
+      } else {
+      console.error('Not Found');
+      }
+    })
+    .catch(console.error);
+};
 // Success -> If the specified Movement exists, console.log it as JSON;
 //              otherwise, console.log "Not Found" and exit.
 // Failure -> Console.error
 
-const update = (id, field, value) => {};
+const update = (id, field, value) => {
+  Movement.findById(id)
+  .then((movement) => {
+    if (movement) {
+      movement.set(field, value);
+      return movement.save();
+    } else {
+      console.error('Not Found');
+    }
+  })
+  .then((movement) => {
+    console.log(JSON.stringify(movement));
+  })
+  .catch(console.error);
+};
 // Success -> If the specified Movement exists, update it and console.log the
 //              updated Movement as JSON; otherwise, console.log "Not Found" and exit.
 // Failure -> Console.error
 
-const destroy = (id) => {};
+const destroy = (id) => {
+  Movement.findById(id)
+    .then((movement) => {
+      if (movement) {
+        movement.remove();
+      } else {
+        console.error('Not Found');
+      }
+    })
+    .catch(console.error);
+};
 // Success -> If the specified Movement exists, destroy it and console.log 'removed';
 //              otherwise, console.log "Not Found" and exit.
 // Failure -> Console.error
