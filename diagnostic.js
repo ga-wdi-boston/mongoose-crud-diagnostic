0f5639c5d2b17aee72adf4b0d5f5e9438a31fa54
akgoode <akgoode@gmail.com>
2017-03-03 09:26:45 -0500

    Add responses to diagnostic
    Comfort: 5
    Clarity: 5
    
    Also added in switch statement which allowed me to run the methods to test if they worked.
    
    Had to NPM install mongoose for this to work.

--- diagnostic.js
+++ diagnostic.js
@@ -31,34 +31,135 @@ mongoose.connect('mongodb://localhost/mongoose-crud-diagnostic');
 const Movement = require('./models/movement.js');
 
 /// ADD YOUR CODE BELOW
+const db = mongoose.connection;
 
-const create = (name, description, startYear, endYear) => {};
+
+const done = function() {
+  db.close();
+};
+
+const create = (name, description, startYear, endYear) => {
+  Movement.create({
+    name: name,
+    description: description,
+    startYear: startYear,
+    endYear: endYear
+  }).then(function(movement) {
+    console.log(movement.toJSON());
+  }).catch(function(error) {
+    console.error(error);
+  }).then(done);
+};
 // Success -> console.log new Movement as JSON
 // Failure -> Console.error
 
-const index = () => {};
+
+const index = () => {
+  let search = {};
+  if(arguments[0] && arguments[1]) {
+    let field = arguments[0];
+    let criterion = arguments[1];
+    if (criterion === '/') {
+      let regex = new RegExp(criterion.slice(1, criterion.length - 1));
+      search[field] = regex;
+    } else {
+      search[field] = criterion;
+    }
+  }
+
+  Movement.find(search).then(function(movements) {
+    movements.forEach(function(movement) {
+      console.log(movement.toJSON());
+    });
+  }).catch(console.error).then(done);
+};
 // Success -> console.log all Movements as JSON
 // Failure -> Console.error
 
-const show = (id) => {};
+const show = (id) => {
+  Movement.findById(id).then(function(movement) {
+    if(movement !== undefined) {
+      console.log(movement.toJSON());
+    } else {
+      console.log('Not Found');
+    }
+  }).catch(console.error).then(done);
+};
 // Success -> If the specified Movement exists, console.log it as JSON;
 //              otherwise, console.log "Not Found" and exit.
 // Failure -> Console.error
 
-const update = (id, field, value) => {};
+const update = (id, field, value) => {
+  Movement.findById(id).then(function(movement) {
+    if (movement !== undefined) {
+      movement.set(field, value);
+      return movement.save();
+    } else {
+      console.log('Not found');
+    }
+  }).then(function(movement) {
+    console.log(movement.toJSON());
+  }).catch(console.error).then(done);
+};
 // Success -> If the specified Movement exists, update it and console.log the
 //              updated Movement as JSON; otherwise, console.log "Not Found" and exit.
 // Failure -> Console.error
 
-const destroy = (id) => {};
+const destroy = (id) => {
+  Movement.findById(id).then(function(movement) {
+    if (movement !== undefined) {
+      return movement.remove();
+    } else {
+      console.log('Not found');
+    }
+  }).catch(console.error).then(done);
+};
 // Success -> If the specified Movement exists, destroy it and console.log 'removed';
 //              otherwise, console.log "Not Found" and exit.
 // Failure -> Console.error
 
-module.exports = {
-  create,
-  index,
-  show,
-  update,
-  destroy
-};
+db.once('open', function() {
+  let command = process.argv[2];
+
+  // Using more than once, avoiding jshint complaints
+  let field;
+  let id;
+
+  switch (command) {
+    case 'create':
+      let name = process.argv[3];
+      let description = process.argv[4];
+      let startYear =  process.argv[5];
+      let endYear =  process.argv[6];
+      let ancestor = process.argv[7];
+      if (true || name) {
+        create(name, description, startYear, endYear, ancestor);
+      } else {
+        console.log('usage c <name> <description> <startYEar> <endYear> <ancestor>');
+        done();
+      }
+      break;
+
+    case `show`:
+      id = process.argv[3];
+      show(id);
+      break;
+
+    case 'update':
+      id = process.argv[3];
+      field = process.argv[4];
+      let value = process.argv[5];
+      update(id, field, value);
+      break;
+
+    case 'destroy':
+      id = process.argv[3];
+      destroy(id);
+      break;
+
+    default:
+      index();
+      break;
+  }
+
+});
